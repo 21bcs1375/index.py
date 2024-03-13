4b38d83c62299354cda694fa7161c2a63a6871b7
Amar Nath <126859814+21bcs1375@users.noreply.github.com>
2024-03-13 14:10:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,3 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
 
-# This is just for running the app locally.
-if __name__ == '__main__':
-    from paste import httpserver
-    httpserver.serve(app, host='127.0.0.1', port='8080')
